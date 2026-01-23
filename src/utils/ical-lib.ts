b67205294f524849bdbfd11491ef19fb13f62b8a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-01-23 02:07:08 +0200

    Clarify date format in toCalDav function
    Updated the comment to clarify the date format for iCal.

--- src/utils/ical-lib.ts
+++ src/utils/ical-lib.ts
@@ -19,7 +19,7 @@ const escapeText = (str: string): string =>
   str.replace(/[\\,;]/g, (match) => `\\${match}`).replace(/\n/g, '\\n');
 
 /**
- * Format a Date to CalDav.
+ * Format a Date as iCal local timestamp (TZID=timezone:YYYYMMDDTHHMMSS).
  */
 const toCalDav = (date: Date): string => {
   // Always output UTC time
